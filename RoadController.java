b71ecc9bec856e9b5dc5e35d3699b012c616c7f0
Joseph Hammana <82621100+Hammana963@users.noreply.github.com>
2022-04-10 21:25:37 -0700

    Update RoadController.java

--- RoadController.java
+++ RoadController.java
@@ -1,87 +1,90 @@
 import java.util.concurrent.Semaphore;
 import java.util.concurrent.TimeUnit;
- 
+
 public class RoadController {
- 
-   // main function
-    public static void main(String[] args) 
-    { 
-       //create new instance of object Road
+
+    // main function
+    public static void main(String[] args)
+    {
+        //create new instance of object Road
         final Road road = new Road();
-         
+
         //Create east village runnable thread
         Thread east = new Thread( new Runnable() {
-             
-         @Override
-         public void run() {
-          
-             while(true)
-             {
-                //create new village object as well as eastVillage thread
-                 Village village = new Village(road);
-                 Thread eastThread = new Thread(village);
-                 // set name and id for east villager
-                 village.setName("East Villager: "+ eastThread.getId());
-                 // start east Thread
-                 eastThread.start();
-                 try
-                 {
-                    // have thread sleep for a random time from 0-10 seconds
-                    long sleepTime = (long)(Math.random()*10);
-                    TimeUnit.SECONDS.sleep(sleepTime);
-                 }
-                 catch(Exception e)
-                 {
-                    // if error, print out exception
-                     System.out.println(e);
-                 }
-             }
-              
-         }
-     });
-      
-      //create west Thread 
-     Thread west = new Thread( new Runnable() {
-          
-         @Override
-         public void run() {
-              
-             while(true)
-             {
-                // create new instance of village and westThread
-                 Village village = new Village(road);
-                 Thread westThread = new Thread(village);
-                 // set name and id for west villager
-                 village.setName("West Villager: "+ westThread.getId());
-                 //start west thread
-                 westThread.start();
-                 try
-                 {
-                    // delay thread by sleeping for a random amount of time 0-10 seconds
-                    long sleepTime = (long)(Math.random()*10);
-                     TimeUnit.SECONDS.sleep(sleepTime);
-                 }
-                 catch(Exception e)
-                 {
-                    // if error print out exception e
-                     System.out.println(e);
-                 }
-             }
-         }
-     });
-         // start both east and west thread for simulation
+
+            @Override
+            public void run() {
+
+                while(true)
+                {
+                    //create new village object as well as eastVillage thread
+                    Village village = new Village(road);
+                    Thread eastThread = new Thread(village);
+                    // set name and id for east villager
+                    village.setName("East Villager: "+ eastThread.getId());
+                    // start east Thread
+                    eastThread.start();
+                    try
+                    {
+                        // have thread sleep for a random time from 0-10 seconds
+                        long sleepTime = (long)(Math.random()*10);
+                        TimeUnit.SECONDS.sleep(sleepTime);
+                        System.out.println(" East Villager: "+ eastThread.getId() + " eating donut for "+ sleepTime + " seconds");
+
+                    }
+                    catch(Exception e)
+                    {
+                        // if error, print out exception
+                        System.out.println(e);
+                    }
+                }
+
+            }
+        });
+
+        //create west Thread
+        Thread west = new Thread( new Runnable() {
+
+            @Override
+            public void run() {
+
+                while(true)
+                {
+                    // create new instance of village and westThread
+                    Village village = new Village(road);
+                    Thread westThread = new Thread(village);
+                    // set name and id for west villager
+                    village.setName("West Villager: "+ westThread.getId());
+                    //start west thread
+                    westThread.start();
+                    try
+                    {
+                        // delay thread by sleeping for a random amount of time 0-10 seconds
+                        long sleepTime = (long)(Math.random()*10);
+                        TimeUnit.SECONDS.sleep(sleepTime);
+                        System.out.println(" West Villager: "+ westThread.getId() + " eating donut for "+ sleepTime + " seconds");
+                    }
+                    catch(Exception e)
+                    {
+                        // if error print out exception e
+                        System.out.println(e);
+                    }
+                }
+            }
+        });
+        // start both east and west thread for simulation
         east.start();
         west.start();
     }
- 
+
 }
- 
+
 // Road class
 class Road
 {
-   // initialize semaphore
+    // initialize semaphore
     private final Semaphore semaphore;
-     
+
     //constructor with semaphore instantiation
     public Road()
     {
@@ -92,7 +95,7 @@ class Road
     {
         try
         {
-           // which villager wants to cross the road
+            // which villager wants to cross the road
             System.out.printf(" %s wants to cross the road.\n", village.getName());
             //acquire semaphore
             semaphore.acquire();
@@ -103,45 +106,45 @@ class Road
         }
         catch(Exception e)
         {
-           // if error, print out exception e
+            // if error, print out exception e
             System.out.println(e);
         }
         finally
         {
-           // Denote that villager has finished crossing the road and release the semaphore
+            // Denote that villager has finished crossing the road and release the semaphore
             System.out.printf(" %s has finished crossing the road.\n",village.getName());
             semaphore.release();
         }
     }
 }
- 
+
 // Runnable class Village
 class Village implements Runnable
 {
-   // Create fields name and road
+    // Create fields name and road
     private String name;
     private Road road;
-     
+
     // village constructor
     public Village(Road road)
     {
         this.road = road;
     }
-     
+
     // run method, calls crossRoad
     public void run()
     {
         road.crossRoad(this);
     }
- 
+
     // getter for name of village
     public String getName() {
         return name;
     }
- 
+
     //setter for name of village
     public void setName(String name) {
         this.name = name;
     }
- 
+
 }
